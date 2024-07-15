cbfabc552e833290d2d3cf931283330393eb41a0
VishnuShreeKrishna <141828803+VishnuShreeKrishna@users.noreply.github.com>
2024-07-15 11:00:00 +0530

    push

---
+++ ToDoList/todo.js
@@ -0,0 +1,18 @@
+// document.write(`Hello this is my todo list`)
+// On app load, get all tasks from localStorage
+window.onload = loadTasks;
+
+function loadTasks() {
+  // Get the tasks from localStorage and convert it to an array
+  let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
+
+  // Loop through the tasks and add them to the list
+  tasks.forEach(task => {
+    const list = document.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check" ${task.completed ? 'checked' : ''}>
+          <input type="text" value="${task.task}" class="task ${task.completed ? 'completed' : ''}" onfocus="getCurrentTask(this)" onblur="editTask(this)">
+          <i class="fa fa-trash" onclick="removeTask(this)"></i>`;
+    list.insertBefore(li, list.children[0]);
+  });
+}
